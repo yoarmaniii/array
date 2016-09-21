b35b6f0b6b0f0202e61c60ab2e10ef34d282504c
Armani Raleigh <armanijolee@gmail.com>
2016-09-21 18:01:07 -0400

    It runssgit status

--- src/arrays.java
+++ src/arrays.java
@@ -1,9 +1,12 @@
 //import your scanner
+import java.util.InputMismatchException;
 import java.util.Scanner;
 public class arrays {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		int userInput;
+		boolean cont = true;
 		String []name = new String[5];
 			name[0]= "Armani";
 			name[1]= "Kristen";
@@ -13,11 +16,51 @@ public class arrays {
 		String []food = new String[5];
 			food[0]= "Salmon";
 			food[1]= "Tacos";
-			
+			food[2]= "Shrimp";
+			food[3]= "Pizza";
+			food[4]= "Pasta";
 		String []city = new String[5];
+			city[0]= "Detroit";
+			city[1]= "Detroit";
+			city[2]= "Detroit";
+			city[3]= "Monroe";
+			city[4]= "Detroit";
+			
+			while(cont){
 		 System.out.println("Welcome! Pick a student! Enter 1-5");
+		 try{
+		 userInput= input.nextInt()-1;}
+		 catch(InputMismatchException e){
+				System.out.println("Thats an invalid prompt, please enter a valid answer! Try Again? Enter- (1)Yes/No");
 		 
-		
+		 System.out.println( "You Picked : " + name[userInput]);
+		 
+		 System.out.println("Choose what you want to know about " + name[userInput] + ":" + " Enter (1) Hometown or (2) Food");
+		 int choice = input.nextInt();
+		 if(choice==1){
+			 System.out.println(name[userInput] + "hometown is " + city[userInput]);
+			 
+		 } //end of if statement
+			  else if(choice==2){
+			 System.out.println( name[userInput] + "favorite food is: " + food[userInput]);
+
+		 }//end of else if statement
+		 
+		 System.out.println("would you like to continue with another student? " + "Enter (1) for Yes or (2) for No");
+		 choice = input.nextInt();
+		 if (choice==1){
+			 cont = true;
+			 
+		 }else if (choice==2){
+			 cont = false;
+			 System.out.println("Goodbye!");
+		 }//end of else if statement for choice 2
+		 
+		 
+		}//end of while loop
+			
+			
+			
 	}//end of main method
 
 }//end of class
