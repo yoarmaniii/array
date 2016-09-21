6ae406e1950aaf2bc2aacde83cc5544972cacca1
Armani Raleigh <armanijolee@gmail.com>
2016-09-21 17:08:40 -0400

    Begining of New Lab 7

---
+++ src/arrays.java
@@ -0,0 +1,23 @@
+//import your scanner
+import java.util.Scanner;
+public class arrays {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String []name = new String[5];
+			name[0]= "Armani";
+			name[1]= "Kristen";
+			name[2]= "Escoe";
+			name[3]= "Tyler";
+			name[4]= "Jeseekia";
+		String []food = new String[5];
+			food[0]= "Salmon";
+			food[1]= "Tacos";
+			
+		String []city = new String[5];
+		 System.out.println("Welcome! Pick a student! Enter 1-5");
+		 
+		
+	}//end of main method
+
+}//end of class
